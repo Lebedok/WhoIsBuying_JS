828f6c3da1625b01a602e8498f0a7c1c16e7af67
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-11-07 13:32:07 -0600

    Update whoBuy.js

--- whoBuy.js
+++ whoBuy.js
@@ -1,3 +1,16 @@
+//WHO is buying lunch? 
+
+//You are going to write a function which will select a random name a list of names. The person selected will have to pay for everybody’s food bill. 
+
+//IMPORTANT: The output should be returned from the function and you do not need ALERT, PROMPT, CONSOLE.LOG . The output should match the example output exactly, including capitalization and punctuation. 
+
+//EXAMPLE INPUT 
+              //[«Angela», «Ben», «Jenny», «Michael», «Chloe»]
+
+//EXAMPLE OUTPUT
+              //Michael is going to buy lunch today! 
+
+
 function whosPaying(names) {
 
     var  numberOfPeople = names.length;
